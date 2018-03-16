3e118c0b809715beaa710a8cdc0dafbdb134361e
evolify <wolf@evolify.cn>
2018-03-16 17:32:03 +0800

    Site updated: 2018-03-16 17:32:02

---
+++ js/ev-nest.js
@@ -0,0 +1,93 @@
+var conf = {
+  count: 150,
+  r1: 85,
+  r2: 150,
+  opacity: .7,
+  color: '#000'
+}
+var canvas, ctx;
+var ww = window.innerWidth;
+var wh = window.innerHeight;
+var nodes = [];
+var cnode = {
+  x: 0,
+  y: 0,
+  dx: 0,
+  dy: 0,
+  r: conf.r2
+};
+window.onload = function() {
+  canvas = document.createElement('canvas');
+  ctx = canvas.getContext('2d');
+  canvas.style.zIndex = -1;
+  canvas.style.position = 'fixed';
+  canvas.style.left=0;
+  canvas.style.top=0;
+  canvas.style.opacity = conf.opacity;
+  canvas.width = ww;
+  canvas.height = wh;
+  document.body.appendChild(canvas);
+  for (let i = 0; i < conf.count; i++) {
+    nodes.push({
+      x: Math.random() * ww,
+      y: Math.random() * wh,
+      dx: Math.random() * 2 - 1,
+      dy: Math.random() * 2 - 1,
+      r: conf.r1
+    })
+  }
+  nodes.push(cnode);
+  render();
+}
+
+function render() {
+  ctx.clearRect(0, 0, ww, wh);
+  nodes.forEach(function(node, index) {
+    ctx.fillRect(node.x - .5, node.y - .5, 1, 1);
+    for (let i = index + 1; i < nodes.length; i++) {
+      let n = nodes[i];
+      let dx = n.x - node.x;
+      let dy = n.y - node.y;
+      let d2 = dx * dx + dy * dy;
+      let r2 = n.r * n.r;
+      let rt = 1 - d2 / r2;
+      if (d2 < r2) {
+        ctx.beginPath();
+        ctx.lineWidth = rt / 2;
+        ctx.strokeStyle = parseColor(conf.color, rt);
+        ctx.moveTo(n.x, n.y);
+        ctx.lineTo(node.x, node.y);
+        ctx.stroke();
+        if (n === cnode && d2 > r2 / 2) {
+          node.x += dx * .02;
+          node.y += dy * .02;
+        }
+      }
+    }
+    node.x += node.dx;
+    node.y += node.dy;
+    node.dx *= node.x > ww || node.x < 0 ? -1 : 1;
+    node.dy *= node.y > wh || node.y < 0 ? -1 : 1;
+  })
+
+  requestAnimationFrame(render);
+}
+
+window.onmousemove = function(event) {
+  const e = event || window.event;
+  cnode.x = e.clientX;
+  cnode.y = e.clientY;
+}
+
+function parseColor(c, a) {
+  let rgba = [0, 0, 0];
+  if (c.length === 4) {
+    rgba = Array.prototype.slice.call(c, 1).map((i) => parseInt(i, 16))
+  } else {
+    rgba[0] = parseInt(c.substr(1, 2), 16) || 0;
+    rgba[1] = parseInt(c.substr(3, 2), 16) || 0;
+    rgba[2] = parseInt(c.substr(5, 2), 16) || 0;
+  }
+  rgba.push(a);
+  return 'rgba(' + rgba.join(',') + ')';
+}
---
+++ js/ev-tag.js
@@ -0,0 +1,60 @@
+let tags = []
+let tagsWrapper
+let tagsWidth = 0,
+    tagsHeight = 0,
+    d = 0
+let activeIndex=-1
+const colors=['#0088ff','#00aa77','#ff5511','#8877ff','#448888','#006688']
+
+function resize() {
+    if (!tagsWrapper) {
+        return
+    }
+    tagsWidth = tagsWrapper.clientWidth
+    tagsHeight = tagsWrapper.clientHeight
+    d = Math.min(tagsWidth, tagsHeight)
+}
+
+function load() {
+    tagsWrapper = document.querySelector('.main .tags')
+    resize()
+    const arr = Array.prototype.slice.call(document.querySelectorAll('.main .content .tags .tag'), 0)
+    const len = arr.length
+    tags = arr.map((tag, i) => {
+        tag.addEventListener('mouseover',()=>{
+            activeIndex=i
+        })
+        tag.addEventListener('mouseout',()=>{
+            activeIndex=-1
+        })
+        return {
+            view: tag,
+            opacity: Math.random(),
+            color:colors[i],
+            ng: i/len*2*Math.PI,
+            ng1:Math.random()*2*Math.PI,
+            x: d/3 * Math.cos(i/len*2*Math.PI),
+            y: d/3 * Math.sin(i/len*2*Math.PI)
+        }
+    })
+    renderTags()
+}
+window.addEventListener('load', load)
+window.addEventListener('resize', resize)
+
+function renderTags() {
+    tags.filter((t,i)=>i!==activeIndex).forEach(tag => {
+        const x=d/3*Math.cos(tag.ng)
+        const y=d/3*Math.sin(tag.ng)
+        const z=d/3*Math.sin(tag.ng1)
+        tag.view.style.color=tag.color
+        tag.view.style.textShadow=`2px 2px 20px ${tag.color}`
+        tag.view.style.transform = `translate3d(${x}px,${y}px,${z}px)`
+        tag.opacity = tag.opacity > 1 ? 0 :
+            tag.opacity < 0 ? 1 :
+            tag.opacity + 0.01
+        tag.ng+=0.007
+        tag.ng1+=0.007
+    })
+    requestAnimationFrame(renderTags)
+}
